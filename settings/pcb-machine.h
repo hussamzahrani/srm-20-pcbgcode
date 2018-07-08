9f8a1c690bf7ec246b27111ac7134ef0e7249588
hussam_zahrani <hussamzahrani@gmail.com>
2018-07-08 12:37:30 +0300

    Initial commit
    All files are modified with default values suitable for SRM-20

---
+++ settings/pcb-machine.h
@@ -0,0 +1,24 @@
+//
+// For ease of use, and to avoid overwritting your settings,
+// use pcb-gcode-setup to make changes to these settings.
+//
+
+real DEFAULT_Z_HIGH = 12.700000;
+real DEFAULT_Z_UP = 2.540000;
+real DEFAULT_Z_DOWN = -0.177800;
+real DRILL_DEPTH = -1.700000;
+real DRILL_DWELL = 1.000000;
+real SPINDLE_ON_TIME = 3.000000;
+real MILLING_DEPTH = -1.500000;
+real TEXT_DEPTH = -0.127000;
+real TOOL_CHANGE_POS_X = 12.700000;
+real TOOL_CHANGE_POS_Y = 15.240000;
+real TOOL_CHANGE_POS_Z = 25.400000;
+real FEED_RATE = 508.000000;
+real FEED_RATE_Z = 254.000000;
+real DEFAULT_WIDTH = 0.100000;
+real X_OFFSET = 0.000000;
+real Y_OFFSET = 0.000000;
+real X_HOME = 0.000000;
+real Y_HOME = 0.000000;
+real EPSILON = 0.002540;
